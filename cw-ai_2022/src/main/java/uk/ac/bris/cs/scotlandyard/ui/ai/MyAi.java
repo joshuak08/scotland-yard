d858aaffb53089e8e216da477067aca037d8878b
joshuak08 <joshuakongxw@gmail.com>
2022-03-30 00:58:27 +0100

    first basic score

--- cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,8 +1,9 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
@@ -37,11 +38,40 @@ public class MyAi implements Ai {
 //	getAvailableMoves
 //  A most simple scoring method would consider the neighbouring nodes to MrX's position and check if detectives are present there
 	public int score(Board board) {
+		int moveScore = 0;
 		var moves = board.getAvailableMoves().asList();
-		for (Move m : moves){
-			if (m.commencedBy(mrX)) return 0;
-			System.out.println(m);
+		GameSetup setup = board.getSetup();
+		Piece.MrX mrX = Piece.MrX.MRX;
+		int mrXLocation;
+// 		Gets mrX location by checking if it's mrX turn, if it is then all moves from getAvailableMoves is by MrX so all source is the same
+		if (moves.get(0).commencedBy().isMrX()) mrXLocation = moves.get(0).source();
+		else return 0;
+		// Gets set of detective pieces and removes mrX
+		Set<Piece> detectivePieces = board.getPlayers().stream().collect(Collectors.toSet());
+		detectivePieces.remove(mrX);
+		// Gets set of type detectives
+		Set<Piece.Detective> detectiveSet = new HashSet<>();
+		for (Piece d : detectivePieces){
+			Piece.Detective detective = Piece.Detective.valueOf(d.toString());
+			detectiveSet.add(detective);
 		}
-		return 0;
+		// Gets location of detective in type Optional<Integer>
+		Set<Optional<Integer>> optionalDetectiveLocation = new HashSet<>();
+		for (Piece.Detective d : detectiveSet){
+			var location = board.getDetectiveLocation(d);
+			optionalDetectiveLocation.add(location);
+		}
+		// Gets location of detective in type Integer
+		Set<Integer> detectiveLocation = new HashSet<>();
+		for (Optional<Integer> l : optionalDetectiveLocation){
+			int location = l.get();
+			detectiveLocation.add(location);
+		}
+		// Checks if nodes adjacent to mrX is occupied by detectives if not increase score
+		Set<Integer> mrXDestinations = new HashSet<>(setup.graph.adjacentNodes(mrXLocation));
+		for (int d : detectiveLocation){
+			if (!mrXDestinations.contains(d)) moveScore++;
+		}
+		return moveScore;
 	}
 }
