69b19e681941817d41f0da73e73573eb98abf70b
joshuak08 <joshuakongxw@gmail.com>
2022-04-04 15:40:16 +0100

    working breath first search

--- cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -9,8 +9,10 @@ import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.graph.ValueGraph;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.*;
 import uk.ac.bris.cs.scotlandyard.model.Move.SingleMove;
@@ -25,33 +27,101 @@ public class MyAi implements Ai {
 			Pair<Long, TimeUnit> timeoutPair) {
 		// returns a random move, replace with your own implementation
 		var moves = board.getAvailableMoves().asList();
-		int mrXLocation;
 		Piece.MrX mrXPiece = Piece.MrX.MRX;
+		GameSetup setup = board.getSetup();
+		int mrXLocation = moves.get(0).source();
+
+		MyGameStateFactory factory = new MyGameStateFactory();
 
 		Set<Piece> detectivePieces = new HashSet<>(board.getPlayers());
 		detectivePieces.remove(mrXPiece);
-		if (moves.get(0).commencedBy().isMrX()) mrXLocation = moves.get(0).source();
-		var detectiveLocations = Objects.requireNonNull(board.getPlayers().stream()
+
+
+		var detectiveLocations = getDetectiveLocations(board);
+		var playerTickets = getPlayerTickets(board);
+
+		var mrXTickets = playerTickets.get(mrXPiece);
+
+		List<Player> detectives = new ArrayList<>();
+		for (Piece d : detectivePieces){
+			Map<ScotlandYard.Ticket,Integer> dTickets = Map.copyOf(playerTickets.get(d));
+			detectives.add(new Player(d, ImmutableMap.copyOf(dTickets), detectiveLocations.get(d)));
+		}
+		Player mrX = new Player(mrXPiece, mrXTickets, mrXLocation);
+
+		MyGameStateFactory.MyGameState state = (MyGameStateFactory.MyGameState) factory.build(setup, mrX, ImmutableList.copyOf(detectives));
+		System.out.println(bfs(board, 13, 67));
+		return moves.get(new Random().nextInt(moves.size()));
+	}
+
+	public int bfs(Board board, int start, int end){
+		GameSetup setup = board.getSetup();
+		int n = setup.graph.nodes().size();
+
+		// solve method from video
+//		Stack<Integer> stack = new Stack<>();
+//		stack.push(start);
+		Deque<Integer> queue = new ArrayDeque<>();
+		queue.push(start);
+		boolean [] visited = new boolean[n];
+		Arrays.fill(visited, false);
+		visited[(start-1)] = true;
+
+		int [] prev = new int[n];
+		while (!queue.isEmpty() && !queue.contains(end)){
+			int node = queue.removeLast();
+			Set<Integer> adjacent = setup.graph.adjacentNodes(node);
+
+			for (int a : adjacent){
+				if (!visited[(a-1)]){
+					queue.push(a);
+					visited[(a-1)] = true;
+					prev[(a-1)] = node;
+				}
+			}
+		}
+
+		// reconstruct path method from video
+		List<Integer> path = new ArrayList<>();
+		for (int at = end; at != 0; at = prev[at-1]){
+			path.add(at);
+		}
+		System.out.println(path);
+		return path.size()-1;
+	}
+
+	public int getDestination(Move move){
+		var destination = move.accept(new Move.Visitor<Integer>() {
+			@Override
+			public Integer visit(SingleMove move) {
+				return move.destination;
+			}
+
+			@Override
+			public Integer visit(Move.DoubleMove move) {
+				return move.destination2;
+			}
+
+		});
+		return destination;
+	}
+
+	public ImmutableMap<Piece.Detective, Integer> getDetectiveLocations(Board board){
+		return Objects.requireNonNull(board.getPlayers().stream()
 				.filter(Piece::isDetective)
 				.map(Piece.Detective.class::cast)
 				.collect(ImmutableMap.toImmutableMap(Function.identity(),
 						x1 -> board.getDetectiveLocation(x1).orElseThrow())));
-		var playerTickets = Objects.requireNonNull(
+	}
+
+	public ImmutableMap<Piece, ImmutableMap<ScotlandYard.Ticket,Integer>> getPlayerTickets (Board board){
+		return Objects.requireNonNull(
 				board.getPlayers().stream().collect(ImmutableMap.toImmutableMap(
 						Function.identity(), x -> {
 							Board.TicketBoard b = board.getPlayerTickets(x).orElseThrow();
 							return Stream.of(ScotlandYard.Ticket.values()).collect(ImmutableMap.toImmutableMap(
 									Function.identity(), b::getCount));
 						})));
-		ImmutableMap<ScotlandYard.Ticket,Integer> mrXTickets = playerTickets.get(mrXPiece);
-		Map<ScotlandYard.Ticket,Integer> detectiveTickets = new HashMap<>();
-		Set<Player> detectives = new HashSet<>();
-		for (Piece d : detectivePieces){
-			Map<ScotlandYard.Ticket,Integer> test = Map.copyOf(playerTickets.get(d));
-			test.forEach((key, value) -> detectiveTickets.put(key, value));
-			detectives.add(new Player(d, ImmutableMap.copyOf(detectiveTickets), detectiveLocations.get(d)));
-		}
-		return moves.get(new Random().nextInt(moves.size()));
 	}
 
 	@Override
@@ -67,42 +137,22 @@ public class MyAi implements Ai {
 //	dist to detectives
 //	getAvailableMoves
 //  A most simple scoring method would consider the neighbouring nodes to MrX's position and check if detectives are present there
-	public Move score(Board board) {
-		int moveScore = 0;
-		var moves = board.getAvailableMoves().asList();
-		GameSetup setup = board.getSetup();
-		Piece.MrX mrX = Piece.MrX.MRX;
-		int mrXLocation;
-// 		Gets mrX location by checking if it's mrX turn, if it is then all moves from getAvailableMoves is by MrX so all source is the same
-		if (moves.get(0).commencedBy().isMrX()) mrXLocation = moves.get(0).source();
-		else return null;
-		// Gets set of detective pieces and removes mrX
-		Set<Piece> detectivePieces = new HashSet<>(board.getPlayers());
-		detectivePieces.remove(mrX);
-		// Gets set of type detectives
-		Set<Piece.Detective> detectiveSet = new HashSet<>();
-		for (Piece d : detectivePieces){
-			Piece.Detective detective = Piece.Detective.valueOf(d.toString());
-			detectiveSet.add(detective);
-		}
-		// Gets location of detective in type Optional<Integer>
-		Set<Optional<Integer>> optionalDetectiveLocation = new HashSet<>();
-		for (Piece.Detective d : detectiveSet){
-			var location = board.getDetectiveLocation(d);
-			optionalDetectiveLocation.add(location);
-		}
-		// Gets location of detective in type Integer
-		Set<Integer> detectiveLocation = new HashSet<>();
-		for (Optional<Integer> l : optionalDetectiveLocation){
-			int location = l.get();
-			detectiveLocation.add(location);
-		}
-		var detectiveLocations = Objects.requireNonNull(board.getPlayers().stream()
-				.filter(Piece::isDetective)
-				.map(Piece.Detective.class::cast)
-				.collect(ImmutableMap.toImmutableMap(Function.identity(),
-						x1 -> board.getDetectiveLocation(x1).orElseThrow())));
+//  Apply dijkstra to all destinations mrX picks destination with largest distance to all detectives
 
+	public int dijkstra(int destination, Set<Player> detectives, Board board){
+		GameSetup setup = board.getSetup();
+		setup.graph.nodeOrder();
+		return 0;
+	}
+
+	public Move score(Board board, Set<Player> detectives){
+		var moves = board.getAvailableMoves();
+		Set<Integer> detectiveLocations = new HashSet<>();
+		detectives.forEach((Player d) -> detectiveLocations.add(d.location()));
+		for (Move m : moves){
+
+		}
 		return null;
 	}
+
 }
